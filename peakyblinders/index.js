bc43949b5dbdb173b656fa197464e6be5f76c431
chicugheorghe <96069660+chicugheorghe@users.noreply.github.com>
2022-03-16 12:14:59 +0200

    2

---
+++ peakyblinders/index.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    let x = document.getElementById("myDIV","myDIV2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction2() {
+    let x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
+
+    ///BUTTON
+
+    function infoButton() {
+        let a = document.getElementById("info");
+       if (a.style.display === "none") {
+       a.style.display = "block";
+
+    }else {
+        a.style.display = "none";
+    }
+}
+
+
+
